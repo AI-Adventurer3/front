f2a1c143d26b99fa9eadd511c28f833a47a5b49e
DESKTOP-0L11UB0\user <olrang.2@gmail.com>
2024-06-13 18:44:30 +0900

    first commit

---
+++ src/pages/IntroPage.js
@@ -0,0 +1,13 @@
+// src/pages/ListPage.js
+import React from 'react';
+
+function IntroPage() {
+  return (
+    <div>
+      <h1>인트로 페이지</h1>
+      <p>인물 감지</p>
+    </div>
+  );
+}
+
+export default IntroPage;
